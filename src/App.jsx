25997bd43253293d7a32d4fc88ae5596baaf087a
TitanCodeXD <wesleysantos32892653@outlook.com>
2023-08-30 21:22:11 -0300

    Criando componentes do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,45 @@
+//React
 import { useState } from 'react'
+
+//CSS
 import './App.css'
+
+//Data
+import {wordsList} from './data/words';
+
+//Components
 import StartScreen from './components/StartScreen/StartScreen'
+import Game from './components/Game/Game';
+import GameOver from './components/GameOver/GameOver';
+
+
+
+
+  const stages = [
+    {id: 1, name: "start"},
+    {id: 2, name: "game"},
+    {id: 3, name: "end"},
+  ];
+
 
 function App() {
- 
+
+  const [gameStage, setgameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  console.log(words);
 
   return (
     <>
       <div className='App'>
-        <StartScreen />
+
+        {gameStage === 'start' && <StartScreen />}
+        {gameStage === 'game' && <Game />}
+        {gameStage === 'end' && <GameOver />}
+        
+
       </div>
+
     </>
   )
 }
---
+++ src/components/Game/Game.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Game.css';
+
+const Game = () => {
+
+  return (
+
+    <div>
+        Game
+    </div>
+  )
+}
+
+export default Game
