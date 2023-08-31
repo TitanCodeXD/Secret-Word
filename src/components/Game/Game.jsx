6430a61d3a3d4a7d353243422538406094ac3a3e
TitanCodeXD <wesleysantos32892653@outlook.com>
2023-08-31 00:46:23 -0300

    Estruturando o componente do jogo

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,14 +1,45 @@
 import React from 'react'
 import './Game.css';
 
+
 const Game = ({verifyLetter}) => {
 
-  return (
+  return ( 
 
-    <div>
-        <h1>Game</h1>
-        <button onClick={verifyLetter}>Finalizar Jogo</button>
+    <div className = "game">
+
+        <p className = "points">
+        <span>Pontuação: 0000</span>
+        </p>
+
+        <h1>Adivinhe a Palavra:</h1>
+
+        <h3 className='Tip'>
+        Dica sobre a palavra: <span>Dica...</span>
+        </h3>
+        
+        <div className='wordContainer'>
+
+        <span className='letter'>A</span>
+        <span className='blackSquare'></span>
+
+        </div>
+
+        <div>
+            <p>Tente adivinhar uma letra da palavra:</p>
+            <form>
+                <input type='text' name= "letter" maxLength="1" required></input>
+                <button>Jogar!</button>
+            </form>
+        </div>
+        <div className='wrongLettersContainer'>
+        <p>Letras já utilizadas</p>
+        <span>A,</span>
+        <span>B,</span>
+        </div>
+
     </div>
+
   )
 }
 
