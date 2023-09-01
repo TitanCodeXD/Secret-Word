82a7cb42f719d84c72037e12ab513530e984f46e
TitanCodeXD <wesleysantos32892653@outlook.com>
2023-08-31 23:17:18 -0300

    Aumentando base de dados de palavras e acrescentando estilizições extras

--- src/components/StartScreen/StartScreen.jsx
+++ src/components/StartScreen/StartScreen.jsx
@@ -10,6 +10,16 @@ const StartScreen = ({startGame}) => {
         <h1>Secret Word</h1>
         <p>Clique no botão abaixo para começar a jogar</p>
         <button onClick={startGame}>Começar o Jogo!</button>
+
+        <div className='creator'>
+          <footer>
+          <p>
+    &copy; Created by
+    <a class = "contact-success"href="https://codepen.io/titancodexd" target="_blank"
+       ><strong> Wesley Roberto dos Santos.</strong></a>
+  </p>
+          </footer>
+        </div>
     </div>
   )
 }
--- src/data/words.js
+++ src/data/words.js
@@ -1,8 +1,12 @@
 export const wordsList = {
-    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
-    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
-    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
-    computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
-    programação: ["Linguagem", "Framework", "JavaScript", "React"],
-    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo", "Lagosta", "Marisco"],
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena", "Volante", "Rádio", "Farol", "Amortecedor", "Radiador", "Freio", "Calota", "Alternador"],
+    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Morango", "Manga", "Abiu", "Abacate", "Butiá", "Damasco", "Figo", "Framboesa", "Graviola", "Goiaba", "Kiwi", "Lima", "Pitaia", "Pistache", "Romã", "Tangerina", "Marmelo"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos", "Cabelo", "Unha", "Dedo", "Garganta", "Mamilos", "Pele", "Peito", "Pescoço", "Umbigo", "Vagina", "Virilha"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Processador"],
+    programação: ["Linguagem", "Framework", "JavaScript", "React", "NodeJS", "HTML", "Ruby"],
+    esporte: ["Esgrima", "Futebol", "Surf", "Voleibol", "Windsurf", "Basquete", "Atletismo", "Airsoft", "Boxe", "Golfe", "Paintball"],
+    objeto: ["Lápis", "Celular", "Urna", "Faca", "Escova", "Copo", "Bola", "Dado", "Tesoura", "Sapato", "Ventilador", "Quadro", "Abajur", "Zarabatana", "Vestido", "Vela", "Garrafa", "Fivela", "Esponja", "Escudo", "Balde", "Alicate", "Mangueira"],
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Lagosta", "Marisco", "Yakisoba", "Waffle", "Empada", "Queijo", "Nachos", "Gelatina", "Batata"],
+    animais: ["Girafa", "Abutre", "Zebu", "Lebre", "Leopardo", "Marmota", "Morcego", "Lince", "Leão", "Ovelha", "Pantera", "Pavão", "Peru", "Quati", "Ratazana", "Surucucu", "Tamanduá", "Tilápia", "Víbora", "Zangão", "Veado", "Tubarão"],
+    profissão: ["Historiador", "Segurança", "Roteirista", "Contador", "Banqueiro", "Oftalmologista", "Jardineiro", "Arquiteto", "Narrador", "Advogado", "Ator", "Urologista", "Quiromante", "Químico", "Pizzaiolo", "Pianista", "Palhaço", "Padeiro"],
   };
